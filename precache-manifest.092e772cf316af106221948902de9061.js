b21a7904f2b77e16e16aa84376c0210fa37312b6
dovantuit <dovantui@gmail.com>
2020-06-05 15:28:46 +0700

    Updates

---
+++ precache-manifest.092e772cf316af106221948902de9061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7d8264e014571d008bbddda4bd7126",
+    "url": "/ToDo_reactjs/index.html"
+  },
+  {
+    "revision": "0cb9ebe9a53d65106cbf",
+    "url": "/ToDo_reactjs/static/css/main.3e09a805.chunk.css"
+  },
+  {
+    "revision": "dfc296259f69f09f0ef7",
+    "url": "/ToDo_reactjs/static/js/2.31c0a666.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo_reactjs/static/js/2.31c0a666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cb9ebe9a53d65106cbf",
+    "url": "/ToDo_reactjs/static/js/main.8ecd8600.chunk.js"
+  },
+  {
+    "revision": "6dcac6b5d1491d6b6aea",
+    "url": "/ToDo_reactjs/static/js/runtime-main.54d0ab30.js"
+  }
+]);
